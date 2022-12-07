2718d10a31b55bd87a295d91bf516b723a92080d
advapp <advappsw@gmail.com>
2022-12-07 15:50:25 -0800

    Added EntrancePass schema, commented in Fee that no schema was found, updated notes

--- NationalParks/Models/EntrancePass.cs
+++ NationalParks/Models/EntrancePass.cs
@@ -2,6 +2,8 @@
 {
     public class EntrancePass
     {
-
+        public string Cost { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
     }
 }
--- NationalParks/Models/Fee.cs
+++ NationalParks/Models/Fee.cs
@@ -2,6 +2,6 @@
 {
     public class Fee
     {
-
+        // As of 2022-12-07 there wasn't a single park entry which had anthing in its "fees" collection
     }
 }
