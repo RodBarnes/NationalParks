8fb3f435eb62b1dd786834fb8c9ebf1969c85b90
advapp <advappsw@gmail.com>
2022-12-19 19:43:56 -0800

    Fixed type on Long/Lat properties

--- NationalParks/Models/WebCam.cs
+++ NationalParks/Models/WebCam.cs
@@ -12,8 +12,8 @@
         public string StatusMessage { get; set; }
         public bool IsStreaming { get; set; }
         public List<string> Tags { get; set; }
-        public long? Latitude { get; set; }
-        public long? Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
         public string GeometryPoiId { get; set; }
         public string Credit { get; set; }
     }
