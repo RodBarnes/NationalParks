faee13426b873df3fb5a21360dbbd123ab743a50
advapp <advappsw@gmail.com>
2022-12-26 16:08:41 -0800

    Renamed Filter to ParkFilter

---
+++ NationalParks/Models/ParkFilter.cs
@@ -0,0 +1,10 @@
+﻿namespace NationalParks.Models
+{
+    public class ParkFilter
+    {
+        public List<Topic> Topics { get; set; } = new();
+        public List<Activity> Activities { get; set; } = new();
+        public List<State> States { get; set; } = new();
+        public List<Amenity> Amenities { get; set; } = new();
+    }
+}
