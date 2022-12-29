13381c00198fb06717b33c0a76eae5a2bed280bb
advapp <advappsw@gmail.com>
2022-12-28 17:04:55 -0800

    Added conveter BoolToTrueFalse

---
+++ NationalParks/Converters/BoolToTrueFalseConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace NationalParks.Converters
+{
+    public class BoolToTrueFalseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isTrue = (bool)value;
+
+            //return isTrue;
+            return isTrue ? "True" : "False";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var strVal = value.ToString();
+
+            return (strVal == "True");
+            //return (strVal == "True");
+        }
+    }
+}
