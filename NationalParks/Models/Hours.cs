a72e84b515d1ce7f11e1051e2e489c63c92062ef
advapp <advappsw@gmail.com>
2022-12-31 12:37:26 -0800

    Enhanced Hours.ToString() to produce more human-readable output.

--- NationalParks/Models/Hours.cs
+++ NationalParks/Models/Hours.cs
@@ -1,18 +1,54 @@
-﻿namespace NationalParks.Models
+﻿using System.Text;
+
+namespace NationalParks.Models
 {
     public class Hours
     {
-        public string Wednesday { get; set; }
         public string Monday { get; set; }
-        public string Thursday { get; set; }
-        public string Sunday { get; set; }
         public string Tuesday { get; set; }
+        public string Wednesday { get; set; }
+        public string Thursday { get; set; }
         public string Friday { get; set; }
         public string Saturday { get; set; }
+        public string Sunday { get; set; }
 
         public override string ToString()
         {
-            return $"Mon:{Monday}\nTue:{Tuesday}\nWed:{Wednesday}\nThu:{Thursday}\nFri:{Friday}\nSat:{Saturday}\nSun:{Sunday}";
+            var sb = new StringBuilder();
+
+            // Key is the value; e.g., "Closed"
+            // Value is abbreviated property name; e.g., Monday
+            var dict = new Dictionary<string, string>();
+
+            CheckForDailyHours(dict, "Mon", Monday);
+            CheckForDailyHours(dict, "Tue", Tuesday);
+            CheckForDailyHours(dict, "Wed", Wednesday);
+            CheckForDailyHours(dict, "Thu", Thursday);
+            CheckForDailyHours(dict, "Fri", Friday);
+            CheckForDailyHours(dict, "Sat", Saturday);
+            CheckForDailyHours(dict, "Sun", Sunday);
+
+            foreach (var key in dict.Keys)
+            {
+                sb.Append($"{dict[key]}: {key}\n");
+            }
+
+            return sb.ToString();
+        }
+
+        private static void CheckForDailyHours(Dictionary<string, string> dict, string propertyName, string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                if (dict.ContainsKey(value))
+                {
+                    dict[value] += $",{propertyName}";
+                }
+                else
+                {
+                    dict.Add(value, propertyName);
+                }
+            }
         }
     }
 }
