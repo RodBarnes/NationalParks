cf40762bf143dede8bebfb8af2734b921053e49c
advapp <advappsw@gmail.com>
2022-12-31 12:00:02 -0800

    Created Models project, moved all model classes, referenced new project in NationalParks, and confirmed all was working.  Added TestProject and confirmed initial tests.

---
+++ TestProject/HoursTest.cs
@@ -0,0 +1,36 @@
+﻿using NationalParks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestClass]
+    public class HoursTest
+    {
+        [TestMethod]
+        public void TestProperties()
+        {
+            var hours = new Hours
+            {
+                Monday = "Monday",
+                Tuesday = "Tuesday",
+                Wednesday = "Wednesday",
+                Thursday = "Thursday",
+                Friday = "Friday",
+                Saturday = "Saturday",
+                Sunday = "Sunday"
+            };
+
+            Assert.AreEqual(hours.Monday, "Monday");
+            Assert.AreEqual(hours.Tuesday, "Tuesday");
+            Assert.AreEqual(hours.Wednesday, "Wednesday");
+            Assert.AreEqual(hours.Thursday, "Thursday");
+            Assert.AreEqual(hours.Friday, "Friday");
+            Assert.AreEqual(hours.Saturday, "Saturday");
+            Assert.AreEqual(hours.Sunday, "Sunday");
+        }
+    }
+}
