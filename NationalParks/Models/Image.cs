5a57d9a70f19aa56fd4a3313a6f8704d62cd4876
advapp <advappsw@gmail.com>
2023-01-03 14:47:47 -0800

    Refactored Image class

--- NationalParks/Models/Image.cs
+++ NationalParks/Models/Image.cs
@@ -2,10 +2,12 @@
 {
     public class Image
     {
+        public string Url { get; set; }
         public string Credit { get; set; }
         public string Title { get; set; }
         public string AltText { get; set; }
+        public string Description { get; set; }
         public string Caption { get; set; }
-        public string Url { get; set; }
+        public MediaCrop[] Crops { get; set; }
     }
 }
