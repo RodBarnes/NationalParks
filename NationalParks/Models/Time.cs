f91b30e28a3b68978a1a2b40f80abe5290591cff
advapp <advappsw@gmail.com>
2023-01-04 11:23:16 -0800

    Added Event classes

---
+++ NationalParks/Models/Time.cs
@@ -0,0 +1,10 @@
+﻿namespace NationalParks.Models
+{
+    public class Time
+    {
+        public string TimeStart { get; set; }
+        public string TimeEnd { get; set; }
+        public string SunsetEnd { get; set; }
+        public string SunriseStart { get; set; }
+    }
+}
