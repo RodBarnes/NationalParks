232596ab1034d187d41ce5230009ac425d3351fa
advapp <advappsw@gmail.com>
2023-01-04 15:22:27 -0800

    Added AppInfo values to About page

--- NationalParks/ViewModels/AboutVM.cs
+++ NationalParks/ViewModels/AboutVM.cs
@@ -2,15 +2,44 @@
 {
     public partial class AboutVM : BaseVM
     {
+        [ObservableProperty]
+        string npsUrl;
+
+        [ObservableProperty]
+        string name;
+
+        [ObservableProperty]
+        string versionString;
+
+        [ObservableProperty]
+        string package;
+
+        [ObservableProperty]
+        string buildString;
+
+        [ObservableProperty]
+        Version version;
+
         public AboutVM()
         {
             Title = "About";
+            NpsUrl = "https://www.nps.gov/subjects/developer/";
+            VersionString = AppInfo.Current.VersionString;
+            Package = AppInfo.Current.PackageName;
+            BuildString = AppInfo.Current.BuildString;
+            Name = AppInfo.Name;
         }
 
         [RelayCommand]
         async Task GoToNpsSite()
         {
             await Shell.Current.DisplayAlert($"NPS", $"Go to NPS developer site", "OK");
+        }
+
+        [RelayCommand]
+        public void GoToAppInfo()
+        {
+            AppInfo.ShowSettingsUI();
         }
     }
 }
