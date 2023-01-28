1bfd573a8ffe602c49a1a716cfde350ece2ce3aa
advapp <advappsw@gmail.com>
2023-01-28 10:28:04 -0800

    Added filter for ThingsToDo

--- NationalParks/Views/ThingToDoFilterPage.xaml.cs
+++ NationalParks/Views/ThingToDoFilterPage.xaml.cs
@@ -2,8 +2,17 @@ namespace NationalParks.Views;
 
 public partial class ThingToDoFilterPage : ContentPage
 {
-	public ThingToDoFilterPage()
+	readonly ThingToDoFilterVM _vm;
+
+	public ThingToDoFilterPage(ThingToDoFilterVM vm)
 	{
 		InitializeComponent();
+		BindingContext = _vm = vm;
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        _vm.PopulateData();
+    }
 }
