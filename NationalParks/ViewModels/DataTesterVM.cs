bff6a5075e1132b93d7bce56dfb7d4f6142ba4d1
advapp <advappsw@gmail.com>
2023-02-04 15:59:11 -0800

    Updated DataTester to use ListVM

--- NationalParks/ViewModels/DataTesterVM.cs
+++ NationalParks/ViewModels/DataTesterVM.cs
@@ -2,35 +2,30 @@
 
 namespace NationalParks.ViewModels;
 
-public partial class DataTesterVM : BaseVM
+public partial class DataTesterVM : ListVM
 {
     DataService dataService;
     IConnectivity connectivity;
+    IGeolocation geolocation;
 
     private int startItems = 0;
     private int limitItems = 500;
     private int totalItems = 1;
     private bool okToContinue = false;
 
-    public ObservableCollection<Models.Place> Places { get; } = new();
-    public ObservableCollection<Models.Tour> Tours { get; } = new();
-
     [ObservableProperty] bool isPopulated = false;
 
     [ObservableProperty] string currentState;
     [ObservableProperty] int currentCount;
     [ObservableProperty] int totalCount;
-
     [ObservableProperty] int matchCount;
-    //[ObservableProperty] int managedByOrgCount;
-    //[ObservableProperty] int isManagedByNpsCount;
-    //[ObservableProperty] int isOpenToPublicCount;
-    //[ObservableProperty] int isMapPinHiddenCount;
-    public DataTesterVM(DataService dataService, IConnectivity connectivity)
+
+    public DataTesterVM(DataService dataService, IConnectivity connectivity, IGeolocation geolocation) : base(connectivity, geolocation)
     {
         Title = "Tester";
         this.dataService = dataService;
         this.connectivity = connectivity;
+        this.geolocation = geolocation;
 
         CurrentState = "Waiting...";
     }
@@ -47,7 +42,7 @@ public partial class DataTesterVM : BaseVM
     {
         okToContinue = true;
         CurrentState = "Running...";
-        //await GetAllPlaces();
+        await GetAllItems();
         if (startItems <= totalItems)
         {
             okToContinue = false;
@@ -56,102 +51,58 @@ public partial class DataTesterVM : BaseVM
     }
 
     [RelayCommand]
-    public void ClearData()
+    public void ClearAllData()
     {
-        Places.Clear();
-        IsPopulated = false;
+        ClearData();
         startItems = 0;
         CurrentState = "Cleared";
         CurrentCount = MatchCount = TotalCount = 0;
     }
 
-    //async Task GetAllPlaces()
-    //{
-    //    try
-    //    {
-    //        IsBusy = true;
-    //        ResultPlaces result;
-    //        string states = "";
+    async Task GetAllItems()
+    {
+        ResultThingsToDo resultDerived;
+        Title = $"Checking {nameof(ResultThingsToDo)}";
+        Term = "thingstodo";
 
-    //        if (Filter is not null)
-    //        {
-    //            // Apply any filters prior to getting the items
-    //            foreach (var state in Filter.States)
-    //            {
-    //                if (states.Length > 0)
-    //                {
-    //                    states += ",";
-    //                }
-    //                states += state.Abbreviation;
-    //            }
-    //        }
+        try
+        {
+            IsBusy = true;
 
-    //        while (totalItems > startItems)
-    //        {
-    //            result = await DataService.GetPlacesAsync(startItems, limitItems, states);
-    //            startItems += result.Data.Count;
-    //            foreach (var place in result.Data)
-    //                Places.Add(place);
-    //            totalItems = result.Total;
-    //            IsPopulated = true;
-    //            TotalCount = totalItems;
-    //            CurrentCount = Places.Count;
-    //            //ManagedByOrgCount = Places.Where(p => !String.IsNullOrEmpty(p.ManagedByOrg)).Count();
-    //            //IsManagedByNpsCount = Places.Where(p => p.IsManagedByNps == 1).Count();
-    //            //IsOpenToPublicCount = Places.Where(p => p.IsOpenToPublic == 1).Count();
-    //            //IsMapPinHiddenCount = Places.Where(p => p.IsMapPinHidden == 1).Count();
-    //            if (!okToContinue)
-    //            {
-    //                break;
-    //            }
-    //        }
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        await Shell.Current.DisplayAlert("Error!", $"{ex.Source}: {ex.Message}", "OK");
-    //    }
-    //    finally
-    //    {
-    //        IsBusy = false;
-    //    }
-    //}
-
-    //async Task GetTours()
-    //{
-    //    try
-    //    {
-    //        IsBusy = true;
-    //        ResultTours result;
-    //        string states = "";
-
-    //        while (totalItems > startItems)
-    //        {
-    //            result = await DataService.GetToursAsync(startItems, limitItems, states);
-    //            startItems += result.Data.Count;
-    //            foreach (var tour in result.Data)
-    //                Tours.Add(tour);
-    //            totalItems = result.Total;
-    //            IsPopulated = true;
-    //            TotalCount = totalItems;
-    //            CurrentCount = Places.Count;
-    //            MatchCount = Places.Where(i => i.Images.Count == 0).Count();
-    //            //ManagedByOrgCount = Places.Where(i => !String.IsNullOrEmpty(p.ManagedByOrg)).Count();
-    //            //IsManagedByNpsCount = Places.Where(i => i.IsManagedByNps == 1).Count();
-    //            //IsOpenToPublicCount = Places.Where(i => i.IsOpenToPublic == 1).Count();
-    //            //IsMapPinHiddenCount = Places.Where(i => i.IsMapPinHidden == 1).Count();
-    //            if (!okToContinue)
-    //            {
-    //                break;
-    //            }
-    //        }
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        await Shell.Current.DisplayAlert("Error!", $"{ex.Source}: {ex.Message}", "OK");
-    //    }
-    //    finally
-    //    {
-    //        IsBusy = false;
-    //    }
-    //}
+            while (totalItems > startItems)
+            {
+                Result result = await DataService.GetItemsAsync(Term, startItems, limitItems);
+                resultDerived = (ResultThingsToDo)result;
+                startItems += resultDerived.Data.Count;
+                foreach (var item in resultDerived.Data)
+                {
+                    if (item.RelatedParks.Count > 1)
+                    {
+                        Items.Add(item);
+                    }
+                }
+                totalItems = result.Total;
+                IsPopulated = true;
+                TotalCount = totalItems;
+                MatchCount = Items.Count;
+                CurrentCount = startItems;
+                //ManagedByOrgCount = Places.Where(p => !String.IsNullOrEmpty(p.ManagedByOrg)).Count();
+                //IsManagedByNpsCount = Places.Where(p => p.IsManagedByNps == 1).Count();
+                //IsOpenToPublicCount = Places.Where(p => p.IsOpenToPublic == 1).Count();
+                //IsMapPinHiddenCount = Places.Where(p => p.IsMapPinHidden == 1).Count();
+                if (!okToContinue)
+                {
+                    break;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            await Shell.Current.DisplayAlert("Error!", $"{ex.Source}: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
 }
