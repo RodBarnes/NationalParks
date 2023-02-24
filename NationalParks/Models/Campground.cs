465c897e0e2ab6deb2f044a842acc38ab963cb97
advapp <advappsw@gmail.com>
2023-02-23 17:19:17 -0800

    Moved Campsite class into file Campground.cs

--- NationalParks/Models/Campground.cs
+++ NationalParks/Models/Campground.cs
@@ -42,3 +42,15 @@ public class Campground : BaseModel
 
     #endregion
 }
+
+public class Campsites
+{
+    public string TotalSites { get; set; }
+    public string Group { get; set; }
+    public string Horse { get; set; }
+    public string TentOnly { get; set; }
+    public string ElectricalHookup { get; set; }
+    public string RvOnly { get; set; }
+    public string WalkBoatTo { get; set; }
+    public string Other { get; set; }
+}
