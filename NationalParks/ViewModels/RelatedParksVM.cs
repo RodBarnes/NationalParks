2793b5b9a37d88258a62b913290060f7d050fa89
advapp <advappsw@gmail.com>
2023-03-04 14:07:33 -0800

    Fixed missing RelatedParks title

--- NationalParks/ViewModels/RelatedParksVM.cs
+++ NationalParks/ViewModels/RelatedParksVM.cs
@@ -1,4 +1,6 @@
-﻿namespace NationalParks.ViewModels;
+﻿using NationalParks.Services;
+
+namespace NationalParks.ViewModels;
 
 public partial class RelatedParksVM : CollapsibleViewVM
 {
@@ -8,5 +10,25 @@ public partial class RelatedParksVM : CollapsibleViewVM
     {
         Items = items;
         HasContent = Items?.Count > 0;
+    }
+
+    [RelayCommand]
+    async Task GoToParkFromParkCode(string parkCode)
+    {
+        Park park;
+
+        ResultParks result = await DataService.GetParkForParkCodeAsync(parkCode);
+        if (result.Data.Count > 0)
+        {
+            park = result.Data.First();
+            await Shell.Current.GoToAsync(nameof(ParkDetailPage), true, new Dictionary<string, object>
+                {
+                    {"Model", park }
+                });
+        }
+        else
+        {
+            await Shell.Current.DisplayAlert("Error!", $"Unable to get park for {parkCode}!", "OK");
+        }
     }
 }
