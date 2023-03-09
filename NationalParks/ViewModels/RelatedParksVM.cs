e20fd378a06bcfda1dc67ff165e18ac2d7935c38
advapp <advappsw@gmail.com>
2023-03-09 09:10:34 -0800

    Removed unused command

--- NationalParks/ViewModels/RelatedParksVM.cs
+++ NationalParks/ViewModels/RelatedParksVM.cs
@@ -1,6 +1,4 @@
-﻿using NationalParks.Services;
-
-namespace NationalParks.ViewModels;
+﻿namespace NationalParks.ViewModels;
 
 public partial class RelatedParksVM : CollapsibleViewVM
 {
@@ -10,26 +8,5 @@ public partial class RelatedParksVM : CollapsibleViewVM
     {
         Items = items;
         HasContent = Items?.Count > 0;
-    }
-
-    [RelayCommand]
-    async Task GoToParkFromParkCode(string parkCode)
-    {
-        Park park;
-
-        ResultParks result = await DataService.GetItemsForParkCodeAsync<ResultParks>(ResultParks.Term, parkCode);
-        if (result.Data.Count > 0)
-        {
-            park = result.Data.First();
-            park.FillMainImage();
-            await Shell.Current.GoToAsync(nameof(ParkDetailPage), true, new Dictionary<string, object>
-                {
-                    {"Model", park }
-                });
-        }
-        else
-        {
-            await Shell.Current.DisplayAlert("Error!", $"Unable to get park for {parkCode}!", "OK");
-        }
     }
 }
