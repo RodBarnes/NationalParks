590782afc5ce09695e370c4e7c291c92bec17fc9
advapp <advappsw@gmail.com>
2023-03-16 16:03:12 -0700

    Moved CodeInfo into separate file

---
+++ NationalParks/CodeInfo.cs
@@ -0,0 +1,31 @@
+﻿namespace NationalParks;
+
+public class CodeInfo
+{
+    public string ObjectName { get; set; }
+    public string MethodName { get; set; }
+
+    public CodeInfo(string objectName, string methodName)
+    {
+        ObjectName = objectName;
+        MethodName = methodName;
+    }
+
+    public CodeInfo(Type typ)
+    {
+        // !!!!!
+        // This expects the type to be the value from MethodBase.GetCurrentMethod().DeclaringType
+        // Anything else will not work because it expects to extract values from the name of this type.
+        // !!!!!
+
+        var str = typ.ToString();
+
+        var objectStart = str.Split('.').Last();
+        var objectEnd = objectStart.IndexOf('+');
+        ObjectName = objectStart[..objectEnd];
+
+        var methodStart = str.Split('<').Last();
+        var methodEnd = methodStart.IndexOf('>');
+        MethodName = methodStart[..methodEnd];
+    }
+}
