6a3d70814dd413881fcee3682927ec1f5365d3c6
advapp <advappsw@gmail.com>
2023-03-17 12:35:57 -0700

    Added DeleteLogs, EmailLogs (stub), NoLogs

--- NationalParks/ViewModels/LogDetailVM.cs
+++ NationalParks/ViewModels/LogDetailVM.cs
@@ -5,6 +5,7 @@ public partial class LogDetailVM : BaseVM
     [ObservableProperty] CollapsibleListVM log1;
     [ObservableProperty] CollapsibleListVM log2;
     [ObservableProperty] CollapsibleListVM log3;
+    [ObservableProperty] bool noLogs;
 
     readonly List<object>[] lists = new List<object>[3];
 
@@ -30,20 +31,49 @@ public partial class LogDetailVM : BaseVM
         if (files.Length > 0)
         {
             Log1 = new CollapsibleListVM($"{Path.GetFileName(files[0])}", false, lists[0]);
+            NoLogs = false;
+        }
+        else
+        {
+            Log1 = new CollapsibleListVM("", false, new List<object>());
+            NoLogs = true;
         }
 
         if (files.Length > 1)
         {
             Log2 = new CollapsibleListVM($"{Path.GetFileName(files[1])}", false, lists[1]);
         }
+        else
+        {
+            Log2 = new CollapsibleListVM("", false, new List<object>());
+        }
 
         if (files.Length > 2)
         {
-            Log3 = new CollapsibleListVM($"{Path.GetFileName(files[2])}", false, lists[3]);
+            Log3 = new CollapsibleListVM($"{Path.GetFileName(files[2])}", false, lists[2]);
         }
         else
         {
             Log3 = new CollapsibleListVM("", false, new List<object>());
         }
+    }
+
+    [RelayCommand]
+    public void DeleteLogs()
+    {
+        var files = Directory.GetFiles(Logger.LogPath, $"{Logger.LogName}*");
+        var nbr = files.Length;
+        for (int i = 0; i < nbr; i++)
+        {
+            Logger.DeleteLog(files[i]);
+        }
+
+        PopulateData();
+    }
+
+    [RelayCommand]
+    public void EmailLogs()
+    {
+        Shell.Current.DisplayAlert("Email", "On their way...", "OK");
     }
 }
