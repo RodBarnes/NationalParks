6c025ab9f2ff9a18c403a3b0afbed980464ba328
advapp <advappsw@gmail.com>
2023-03-31 13:58:31 -0700

    Added copyright notice

--- NationalParks/Pages/TourDetailPage.xaml.cs
+++ NationalParks/Pages/TourDetailPage.xaml.cs
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2022 Rod Barnes
+ * See the LICENSE.txt file in the project root for specific restrictions.
+ */
 namespace NationalParks.Pages;
 
 public partial class TourDetailPage : ContentPage
